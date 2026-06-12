d7c330576a39fce54253db24a47278151213685a
Trisha Pandey <trisha.pandey.cc@gmail.com>
2026-06-12 12:25:05 +0530

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Auth from "./pages/Auth"
 import Checkout from "./pages/Checkout"
 import Navbar from './Component/Navbar'
 import './App.css'
+import AuthProvider from './context/AuthContext'
 
 function App() {
 
 
   return (
+  <AuthProvider>
    <div className="app">
     <Navbar />
     <Routes>
@@ -18,6 +20,7 @@ function App() {
       <Route path='/checkout'element={<Checkout />} />
     </Routes>
     </div>
+  </AuthProvider>
   );
 }
 
